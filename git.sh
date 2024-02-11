90724fd00367e3cdfd031b850df570167fcd1ef9
houji <1823523855@qq.com>
2024-02-11 23:03:23 +0800

    my_muduo has completed.

---
+++ git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git status
+git add .
+git commit -m "my_muduo has completed." # change to your message
+git push origin master:main # local repository's master branch to remote repository's main branch
